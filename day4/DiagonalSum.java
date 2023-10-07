c42c281c950dfcc9edd29131e160fd03d1ae9bc9
Arishtotle <107453990+Arishtotle@users.noreply.github.com>
2023-10-07 23:50:17 +0530

    Add files via upload

---
+++ day4/DiagonalSum.java
@@ -0,0 +1,33 @@
+ public class DiagonalSum {
+	
+	public static int Dia(int [][] m) {
+		int sum=0;
+		int L=m.length;
+		for(int i=0;i<L;i++) {
+			sum=sum+m[i][i];
+			sum=sum+m[i][L-1-i];
+		
+		}
+		if(L%2 !=0)
+		{
+		sum=sum-m[L/2][L/2];
+		return sum;
+		
+		}
+		return L;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+	int	m[][] = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+	
+	System.out.print(Dia(m));
+		}
+		
+		}
+		
